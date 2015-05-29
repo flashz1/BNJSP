dc2364d799e760b0e7fff1c97d151b9920094b55
Cara Valerii <flash.sk@gmail.com>
2015-05-30 00:13:38 +0300

    mvc

--- server.js
+++ server.js
@@ -1,140 +1,21 @@
-var express = require('express');
-var path = require('path');
-var request = require('request');
-var log = require('./libs/log.js')(module);
-var config = require('./libs/config');
-//var ArticleModel = require('./libs/mongoose').ArticleModel;
-var app = express();
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    favicon = require('serve-favicon'),
+    log = require('./utils/log.js')(module),
+    config = require('./config'),
+    app = express();
 
-app.use('/css', express.static(__dirname + '/css'));
-app.use("/libs", express.static(__dirname + '/libs'));
-app.use("/js", express.static(__dirname + '/js'));
-app.use("/img", express.static(__dirname + '/img'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('my secret here'));
+app.use(express.static(__dirname + '/public'));
 
-app.set('views', __dirname + '/tpl');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.render('page', { title: 'Basic Node.js Project - HOME', headerImage: '/img/headerbg.jpg' });
-});
-
-app.get('/login', function (req, res) {
-    res.render('login', { title: 'Basic Node.js Project - LOGIN', headerImage: '/img/loginbg.jpg' });
-});
-
-app.get('/api/articles', function(req, res) {
-    return ArticleModel.find(function (err, articles) {
-        if (!err) {
-            return res.send(articles);
-        } else {
-            res.statusCode = 500;
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
-            return res.send({ error: 'Server error' });
-        }
-    });
-});
-
-app.post('/api/articles', function(req, res) {
-    var article = new ArticleModel({
-        title: req.body.title,
-        author: req.body.author,
-        description: req.body.description,
-        images: req.body.images
-    });
-
-    article.save(function (err) {
-        if (!err) {
-            log.info("article created");
-            return res.send({ status: 'OK', article:article });
-        } else {
-            console.log(err);
-            if(err.name == 'ValidationError') {
-                res.statusCode = 400;
-                res.send({ error: 'Validation error' });
-            } else {
-                res.statusCode = 500;
-                res.send({ error: 'Server error' });
-            }
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
-        }
-    });
-});
-
-app.get('/api/articles/:id', function(req, res) {
-    return ArticleModel.findById(req.params.id, function (err, article) {
-        if(!article) {
-            res.statusCode = 404;
-            return res.send({ error: 'Not found' });
-        }
-        if (!err) {
-            return res.send({ status: 'OK', article:article });
-        } else {
-            res.statusCode = 500;
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
-            return res.send({ error: 'Server error' });
-        }
-    });
-});
-
-app.put('/api/articles/:id', function (req, res){
-    return ArticleModel.findById(req.params.id, function (err, article) {
-        if(!article) {
-            res.statusCode = 404;
-            return res.send({ error: 'Not found' });
-        }
-
-        article.title = req.body.title;
-        article.description = req.body.description;
-        article.author = req.body.author;
-        article.images = req.body.images;
-        return article.save(function (err) {
-            if (!err) {
-                log.info("article updated");
-                return res.send({ status: 'OK', article:article });
-            } else {
-                if(err.name == 'ValidationError') {
-                    res.statusCode = 400;
-                    res.send({ error: 'Validation error' });
-                } else {
-                    res.statusCode = 500;
-                    res.send({ error: 'Server error' });
-                }
-                log.error('Internal error(%d): %s',res.statusCode,err.message);
-            }
-        });
-    });
-});
-
-app.delete('/api/articles/:id', function (req, res){
-    return ArticleModel.findById(req.params.id, function (err, article) {
-        if(!article) {
-            res.statusCode = 404;
-            return res.send({ error: 'Not found' });
-        }
-        return article.remove(function (err) {
-            if (!err) {
-                log.info("article removed");
-                return res.send({ status: 'OK' });
-            } else {
-                res.statusCode = 500;
-                log.error('Internal error(%d): %s',res.statusCode,err.message);
-                return res.send({ error: 'Server error' });
-            }
-        });
-    });
-});
-
-app.use(function(req, res, next){
-    res.status(404);
-    log.debug('Not found URL: %s',req.url);
-    res.render('404', { title: 'Page not found' });
-});
-
-app.use(function(err, req, res, next){
-    res.status(err.status || 500);
-    log.error('Internal error(%d): %s',res.statusCode,err.message);
-    res.send({ error: err.message });
-});
+//app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(require('./controllers'));
 
 app.listen(config.get('port'), function(){
     log.info('Express server listening on port ' + config.get('port'));
