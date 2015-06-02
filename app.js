6775a724c3a24ad7ef2a4c53214ace86139c68ed
Cara Valerii <flash.sk@gmail.com>
2015-06-02 23:53:45 +0300

    fix

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use(require('./controllers'));
 db.once('open', function () {
     console.log('Connected to DB: ' + config.get('db:dbName'));
     app.listen(config.get('server:port'), function(){
-        log.debug('Express server listening on port ' + config.get('server:port'));
+        log.info('Express server listening on port ' + config.get('server:port'));
     });
 });
--- controllers/index.js
+++ controllers/index.js
@@ -6,8 +6,13 @@ var express = require('express'),
 router.use('/contacts', require('./contacts'));
 
 router.get('/', function(req, res) {
-        req.session.numberOfVizits = req.session.numberOfVizits + 1 || 1;
-        res.render('index', {vizits: req.session.numberOfVizits});
+    var users = UsersModel.find({}, function(err, users){
+       if(err) return console.log(err);
+        return users;
+    });
+    req.session.numberOfVizits = req.session.numberOfVizits + 1 || 1;
+    res.render('index', {vizits: req.session.numberOfVizits, users: users});
+
     });
 //router.get('/:name', function(req, res){
 //        var  newUser = new UsersModel({
